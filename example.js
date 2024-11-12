0fb5054d9575b9dcbb1b5c1f75f341f62bd7ac34
Sudharshini56 <bala.sudharshini@gmail.com>
2024-11-12 18:28:03 +0530

    example.js

--- example.js
+++ example.js
@@ -3,3 +3,24 @@
 
 let colors = ["black", "red", "blue", "white"]
 console.log(colors)
+console.log(colors.length)
+console.log(colors.length - 1)
+console.log(colors[0])
+console.log(colors[colors.length - 1])
+
+//add new element
+//mix of int ans string 
+let array = [5, 6, 7, 8, 'a', "sudharshini", [100, 200]]
+console.log(array)
+console.log(array[4])
+console.log(array[5])
+console.log(array[6])
+console.log(array[6][0])
+
+// 2d array
+let mix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+console.log(mix)
