183885b33469cb3a90bc4e0d68cd9ba3072b5887
Sudharshini56 <bala.sudharshini@gmail.com>
2024-11-14 12:52:46 +0530

    example4.js

--- example4.js
+++ example4.js
@@ -1,4 +1,15 @@
 //String to Array
-let string = 'Sudharshini,SoftwareEngineer';
-let array = string.join()
-console.log(string)
+let convertArray = ['Sudharshini,SoftwareEngineer']
+let convertString = 'Sudharshini,SoftwareEngineer';
+convertString = convertArray.join()
+console.log(convertString)
+
+convertArray = convertString.split(',')
+console.log(convertArray)
+
+convertArray.slice()
+console.log(convertArray)
+
+
+let convertArr = ['Sudharshini', 'SoftwareEngineer']
+console.log(convertArr.slice(0, 1))
